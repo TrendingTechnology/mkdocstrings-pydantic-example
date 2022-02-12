8639769628c30fbd2e64be76bb4d3eb86d701230
SupImDos <SupImDos@gmail.com>
2022-02-12 15:55:02 +0800

    Make even more minimal.

--- example/__init__.py
+++ example/__init__.py
@@ -2,11 +2,11 @@
 
 import pydantic
 
-def fn(arg: pydantic.fields.ModelField) -> None:
+def fn(arg: pydantic.BaseModel) -> None:
     """Function docstring
 
     Args:
-        arg (pydantic.fields.ModelField): This is going to break!
+        arg (pydantic.BaseModel): This is going to break!
     """
     # This bit doesn't matter
     print(arg)
